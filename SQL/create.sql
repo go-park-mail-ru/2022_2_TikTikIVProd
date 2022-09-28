24ffa4ae3bf6db416764b86f93ea21d579865602
p1xel <dima.neumoin1234@gmail.com>
2022-09-28 03:27:22 +0300

    add sql and makefile

---
+++ SQL/create.sql
@@ -0,0 +1,31 @@
+CREATE TABLE IF NOT EXISTS images (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	img_link TEXT NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS users (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	first_name VARCHAR(32) NOT NULL,
+	last_name VARCHAR(32) NOT NULL,
+	nick_name VARCHAR(10) NOT NULL,
+	mobile VARCHAR(16) NOT NULL,
+	avatar_img_id INT REFERENCES images(id),
+	email VARCHAR(50) NOT NULL,
+	passhash VARCHAR(50) NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS user_posts (
+	id INT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+	user_id INT REFERENCES users(id),
+	message TEXT,
+	create_date DATE NOT NULL
+);
+
+
+CREATE TABLE IF NOT EXISTS user_posts_images (
+	user_post_id INT REFERENCES user_posts(id),
+	img_id INT REFERENCES images(id),
+	PRIMARY KEY (user_post_id, img_id)
+);
