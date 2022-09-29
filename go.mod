15ebc53a304c886c08096304347690bc0f12a40f
p1xel <dima.neumoin1234@gmail.com>
2022-09-29 03:42:41 +0300

    done

---
+++ go.mod
@@ -0,0 +1,5 @@
+module 2022_2_TikTikIVProd
+
+go 1.19
+
+require github.com/gorilla/mux v1.8.0 // indirect
---
+++ main_test.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"strings"
+	"testing"
+)
+
+func Test_Feed(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://localhost:8080/feed", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	Feed(res, req)
+
+	exp, _ := json.Marshal("Feed") // прост потому что в хендлере тоже json от строки делает "Feed"
+	act := res.Body.Bytes()
+	if strings.Trim(string(act), "\n") != string(exp) {
+		t.Fatalf("Expected %s got %s", string(exp), string(act))
+	}
+}
+
+func Test_SignIn(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://localhost:8080/feed", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	SignIn(res, req)
+
+	exp, _ := json.Marshal("SignIn") // прост потому что в хендлере тоже json от строки делает "Feed"
+	act := res.Body.Bytes()
+	if strings.Trim(string(act), "\n") != string(exp) {
+		t.Fatalf("Expected %s got %s", string(exp), string(act))
+	}
+}
+
+func Test_SignUp(t *testing.T) {
+	req, err := http.NewRequest("GET", "http://localhost:8080/feed", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	res := httptest.NewRecorder()
+	SignUp(res, req)
+
+	exp, _ := json.Marshal("SignUp") // прост потому что в хендлере тоже json от строки делает "Feed"
+	act := res.Body.Bytes()
+	if strings.Trim(string(act), "\n") != string(exp) {
+		t.Fatalf("Expected %s got %s", string(exp), string(act))
+	}
+}
