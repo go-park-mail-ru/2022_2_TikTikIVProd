e6861cd72086395046b5fda8d9dfc82336a4f048
Kuznetsova Anastasia <kn230902@gmail.com>
2022-09-30 15:24:00 +0300

    fix router

--- app/router/router.go
+++ app/router/router.go
@@ -17,24 +17,23 @@ func NewRouter(/*будут переданы данные какие то*/) *Ro
 		Router: mux.NewRouter(),
 	}
 
-	r.HandleFunc("/feed", Feed)
-	r.HandleFunc("/signin", SignIn)
-	r.HandleFunc("/signup", SignUp)
+	r.HandleFunc("/feed", r.Feed)
+	r.HandleFunc("/signin", r.SignIn)
+	r.HandleFunc("/signup", r.SignUp)
 	return r
 }
 
-func SignIn(w http.ResponseWriter, r *http.Request) {
+func (router *Router) SignIn(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode("SignIn")
 }
 
-func SignUp(w http.ResponseWriter, r *http.Request) {
+func (router *Router) SignUp(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode("SignUp")
 }
 
-func Feed(w http.ResponseWriter, r *http.Request) {
+func (router *Router) Feed(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode("Feed")
 }
-
