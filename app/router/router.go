413d11c20263fd4a3d4a89a901cfe2c98959eada
Kuznetsova Anastasia <kn230902@gmail.com>
2022-10-03 21:11:56 +0300

    fix errors

--- app/router/router.go
+++ app/router/router.go
@@ -25,8 +25,8 @@ type Router struct {
 	UserRouter
 }
 
-func NewRouter(ur *usersRep.UsersRep) *RouterUser {
-	r := &Router {
+func NewRouter(ur *usersRep.UsersRep) *UserRouter {
+	r := &UserRouter {
 		Router: mux.NewRouter(),
 		ur: ur,
 	}
@@ -38,13 +38,9 @@ func NewRouter(ur *usersRep.UsersRep) *RouterUser {
 }
 
 
-func (router *RouterUser) SignUp(w http.ResponseWriter, r *http.Request) {
+func (router *UserRouter) SignUp(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-			Message: "incorrect http method",
-		})
-		http.Error(w, "bad request", http.StatusMethodNotAllowed)
+		http.Error(w, "incorrect http method", http.StatusMethodNotAllowed)
 		return
 	}
 
@@ -53,35 +49,19 @@ func (router *RouterUser) SignUp(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-			Message: err.Error(),
-		})
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
 	createdUser, err := router.ur.CreateUser(user)
 	if err.Error() == "nickname " + user.NickName + "already in use." {
-		w.WriteHeader(http.StatusConflict) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-										Message: err.Error(),
-									})
-		http.Error(w, "nickname already in use", http.StatusConflict)
+		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	} else if err.Error() == "user with email " + user.Email + "already exists." {
-		w.WriteHeader(http.StatusConflict) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-										Message: err.Error(),
-									})
-		http.Error(w, "user exists", http.StatusConflict)
+		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	} else if err != nil {
-		w.WriteHeader(http.StatusInternalServerError) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-			Message: err.Error(),
-		})
-		http.Error(w, "not created", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
 		w.WriteHeader(http.StatusCreated)
@@ -91,21 +71,14 @@ func (router *RouterUser) SignUp(w http.ResponseWriter, r *http.Request) {
 										Body: createdUser,
 									})
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError) //Надо ли?
 		http.Error(w, "encode error", http.StatusInternalServerError)
 		return
 	}
 }
 
-
-
 func (router *UserRouter) SignIn(w http.ResponseWriter, r *http.Request) {
 	if r.Method != http.MethodPost {
-		w.WriteHeader(http.StatusMethodNotAllowed) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-			Message: "incorrect http method",
-		})
-		http.Error(w, "bad request", http.StatusMethodNotAllowed)
+		http.Error(w, "incorrect http method", http.StatusMethodNotAllowed)
 		return
 	}
 	
@@ -114,51 +87,57 @@ func (router *UserRouter) SignIn(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 	err := json.NewDecoder(r.Body).Decode(&user)
 	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		json.NewEncoder(w).Encode(MessageError {
-			Message: err.Error(),
-		})
 		http.Error(w, "bad request", http.StatusBadRequest)
 		return
 	}
 
 	gotUser, err := router.ur.SignIn(user)
 	if err.Error() == "can't find user with email " + user.Email {
-		w.WriteHeader(http.StatusConflict) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-										Message: err.Error(),
-									})
-		http.Error(w, "user doesn't exist", http.StatusConflict)
+		http.Error(w, err.Error(), http.StatusConflict)
 		return
 	} else if err.Error() == "incorrect password" {
-		w.WriteHeader(http.StatusUnauthorized) //Надо ли?
-		json.NewEncoder(w).Encode(MessageError {
-										Message: err.Error(),
-									})
-		http.Error(w, "user doesn't exist", http.StatusUnauthorized)
+		http.Error(w, err.Error(), http.StatusUnauthorized)
 		return
 	} else if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(MessageError {
-			Message: err.Error(),
-		})
-		http.Error(w, "server error", http.StatusInternalServerError)
+		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	} else {
 		w.WriteHeader(http.StatusOK)
 	}
+
+
+	
+	// Create a new random session token
+	// we use the "github.com/google/uuid" library to generate UUIDs
+	sessionToken := uuid.NewString()
+	expiresAt := time.Now().Add(120 * time.Second)
+
+	// Set the token in the session map, along with the session information
+	sessions[sessionToken] = session{
+		username: creds.Username,
+		expiry:   expiresAt,
+	}
+
+	// Finally, we set the client cookie for "session_token" as the session token we just generated
+	// we also set an expiry time of 120 seconds
+	http.SetCookie(w, &http.Cookie{
+		Name:    "session_token",
+		Value:   sessionToken,
+		Expires: expiresAt,
+	})
+
+
 	
 	err = json.NewEncoder(w).Encode(Response {
 										Body: gotUser,
 									})
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError) //Надо ли?
 		http.Error(w, "encode error", http.StatusInternalServerError)
 		return
 	}
 }
 
-func (router *Router) Feed(w http.ResponseWriter, r *http.Request) {
+func (router *UserRouter) Feed(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode("Feed")
 }
