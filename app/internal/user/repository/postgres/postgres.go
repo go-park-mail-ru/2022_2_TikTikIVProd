31b256411e2fbb1d2ef05dfa1798dcd4694e2b0c
Kuznetsova Anastasia <kn230902@gmail.com>
2022-10-07 17:57:22 +0300

    fix auth

--- app/internal/user/repository/postgres/postgres.go
+++ app/internal/user/repository/postgres/postgres.go
@@ -1,7 +1,10 @@
 package postgres
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 
 	"github.com/go-park-mail-ru/2022_2_TikTikIVProd/internal/user/model"
 	"github.com/go-park-mail-ru/2022_2_TikTikIVProd/internal/user/repository"
@@ -56,11 +59,14 @@ func (dbUsers *dataBase) SelectUserByEmail(email string) (*model.User, error) {
 func (dbUsers *dataBase) CreateUser(u model.User) (*model.User, error) {
 	user := model.User{}
 
+	fmt.Println(u)
 	tx := dbUsers.db.Table("users").Raw("INSERT INTO users (first_name, last_name, nick_name, email, passhash) VALUES (?, ?, ?, ?, ?) RETURNING *",
 			u.FirstName, u.LastName, u.NickName, u.Email, u.Password).Scan(&user)
 	if tx.Error != nil {
 		return nil, tx.Error
 	}
+
+	fmt.Println(user)
 
 	return &user, nil
 }
@@ -68,22 +74,22 @@ func (dbUsers *dataBase) CreateUser(u model.User) (*model.User, error) {
 func (dbUsers *dataBase) CreateCookie(c model.Cookie) (*model.Cookie, error) {
 	cookie := model.Cookie{}
 
-	tx := dbUsers.db.Table("cookies").Raw("INSERT INTO cookies VALUES (?, ?, ?) RETURNING *",
-			c.SessionToken, c.UserId, c.Expires).Scan(&cookie)
-	if tx.Error != nil {
-		return nil, tx.Error
+	row := dbUsers.db.Table("cookies").Create(&c).Clauses(clause.Returning{}).Row()
+	err := row.Scan(&cookie.SessionToken, &cookie.UserId, &cookie.Expires)
+	if err != nil {
+		return nil, err
 	}
-
+	
 	return &cookie, nil
 }
 
 func (dbUsers *dataBase) SelectCookie(value string) (*model.Cookie, error) {
 	cookie := model.Cookie{}
 
-	row := dbUsers.db.Table("cookies").Raw("SELECT * FROM cookies WHERE value = ?", value). Scan(&cookie)
-
-	if row.Error != nil {
-		return nil, row.Error
+	row := dbUsers.db.Table("cookies").Where("value = ?", value).Row()
+	err := row.Scan(&cookie.SessionToken, &cookie.UserId, &cookie.Expires)
+	if err != nil {
+		return nil, err
 	}
 
 	return &cookie, nil
