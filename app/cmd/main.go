81c9b86ef9f6966b360197630c6fd6f69a017993
p1xel <dima.neumoin1234@gmail.com>
2022-10-24 03:16:11 +0300

    prod cfg

--- app/cmd/main.go
+++ app/cmd/main.go
@@ -28,12 +28,12 @@ import (
 // @version 1.0
 // @host 89.208.197.127:8080
 
-var testCfg = postgres.Config{DSN: "host=localhost user=postgres password=postgres port=13080"}
+//var testCfg = postgres.Config{DSN: "host=localhost user=postgres password=postgres port=13080"}
 
-//var prod_cfg = postgres.Config{DSN: "host=ws_pg user=postgres password=postgres port=5432"}
+var prod_cfg = postgres.Config{DSN: "host=ws_pg user=postgres password=postgres port=5432"}
 
 func main() {
-	db, err := gorm.Open(postgres.New(testCfg),
+	db, err := gorm.Open(postgres.New(prod_cfg),
 		&gorm.Config{})
 
 	if err != nil {
@@ -54,6 +54,7 @@ func main() {
 	imageUC := imageUsecase.NewImageUsecase(imageDB)
 
 	e := echo.New()
+	//e.Use(middleware.Logger())
 
 	_postsDelivery.NewDelivery(e, postsUC)
 	_usersDelivery.NewDelivery(e, usersUC)
