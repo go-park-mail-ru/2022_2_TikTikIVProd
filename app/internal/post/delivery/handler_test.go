e71f42b686df8f54c22887969c38f98190f22ecf
p1xel <dima.neumoin1234@gmail.com>
2022-11-10 00:30:10 +0300

    some test

--- app/internal/post/delivery/handler_test.go
+++ app/internal/post/delivery/handler_test.go
@@ -1,6 +1,8 @@
 package delivery_test
 
 import (
+	"encoding/json"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"strconv"
@@ -17,9 +19,16 @@ import (
 )
 
 type TestCaseGetPost struct {
-	ArgData string
-	Error error
+	ArgData    string
+	Error      error
 	StatusCode int
+}
+
+type TestCaseCreatePost struct {
+	ArgDataBody    string
+	ArgDataContext int
+	Error          error
+	StatusCode     int
 }
 
 func TestDeliveryGetPost(t *testing.T) {
@@ -40,16 +49,16 @@ func TestDeliveryGetPost(t *testing.T) {
 	e := echo.New()
 	postDelivery.NewDelivery(e, mockUCase)
 
-	cases := map[string]TestCaseGetPost {
+	cases := map[string]TestCaseGetPost{
 		"success": {
-			ArgData: strconv.Itoa(post.ID),
-			Error: nil,
+			ArgData:    strconv.Itoa(post.ID),
+			Error:      nil,
 			StatusCode: http.StatusOK,
 		},
 		"bad_request": {
 			ArgData: postIdBadRequest,
 			Error: &echo.HTTPError{
-				Code: http.StatusNotFound,
+				Code:    http.StatusNotFound,
 				Message: "Post not found",
 			},
 		},
@@ -59,7 +68,7 @@ func TestDeliveryGetPost(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			req := httptest.NewRequest(echo.GET, "/post/:id", strings.NewReader(""))
 			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
-			
+
 			rec := httptest.NewRecorder()
 			c := e.NewContext(req, rec)
 			c.SetPath("/post/:id")
@@ -78,9 +87,58 @@ func TestDeliveryGetPost(t *testing.T) {
 	mockUCase.AssertExpectations(t)
 }
 
+func TestDeliveryCreatePost(t *testing.T) {
+	mockPost := models.Post{Message: "123", Images: []models.Image{}}
 
+	jsonPost, err := json.Marshal(mockPost)
+	fmt.Println(string(jsonPost))
+	assert.NoError(t, err)
 
+	mockUCase := mocks.NewPostUseCaseI(t)
 
+	mockUCase.On("CreatePost", &mockPost).Return(nil)
 
+	handler := postDelivery.Delivery{
+		PUsecase: mockUCase,
+	}
 
+	e := echo.New()
+	postDelivery.NewDelivery(e, mockUCase)
 
+	cases := map[string]TestCaseCreatePost{
+		"success": {
+			ArgDataBody:    string(jsonPost),
+			ArgDataContext: mockPost.UserID,
+			Error:          nil,
+			StatusCode:     http.StatusOK,
+		},
+		//"bad_request": {
+		//	ArgDataBody: "sffvfb",
+		//	Error: &echo.HTTPError{
+		//		Code:    http.StatusBadRequest,
+		//		Message: models.ErrBadRequest.Error(),
+		//	},
+		//},
+	}
+
+	for name, test := range cases {
+		t.Run(name, func(t *testing.T) {
+			req := httptest.NewRequest(echo.POST, "/post/create", strings.NewReader(test.ArgDataBody))
+			req.Header.Set(echo.HeaderContentType, echo.MIMEApplicationJSON)
+
+			rec := httptest.NewRecorder()
+			c := e.NewContext(req, rec)
+			c.SetPath("/post/create")
+			c.Set("user_id", test.ArgDataContext)
+
+			err := handler.CreatePost(c)
+			require.Equal(t, test.Error, err)
+
+			if err == nil {
+				assert.Equal(t, test.StatusCode, rec.Code)
+			}
+		})
+	}
+
+	mockUCase.AssertExpectations(t)
+}
